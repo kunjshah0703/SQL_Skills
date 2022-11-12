cb786aa93a1a601c783ddc09bc6f6b1e1cc89d88
kunjshah0703 <72154374+kunjshah0703@users.noreply.github.com>
2022-11-12 14:47:54 +0530

    Add files via upload
    Use case of UPDATE And DELETE in SQL and Queries.

---
+++ SQL_UPDDEL_Queries.sql
@@ -0,0 +1,21 @@
+/*
+Updating/Deleting Data
+*/
+
+--UPDATE EmployeeDemographics
+--SET EmployeeID = 1012
+--WHERE FirstName = 'Holly' AND LastName = 'Flax';
+
+--SELECT *
+--FROM EmployeeDemographics;
+
+--UPDATE EmployeeDemographics
+--SET Age = 31, Gender = 'Female'
+--WHERE FirstName = 'Holly' AND LastName = 'Flax';
+
+
+--DELETE FROM EmployeeDemographics
+--WHERE EmployeeID = 1005;
+
+--SELECT *
+--FROM EmployeeDemographics;
