7b1fd994022f60daabef56875c7680977538dfd9
kunjshah0703 <72154374+kunjshah0703@users.noreply.github.com>
2022-11-15 19:13:16 +0530

    Add files via upload

---
+++ SQL_CTE_Queryies.sql
@@ -0,0 +1,12 @@
+-- Common Table Expressions
+
+--WITH CTE_Employee AS 
+--(SELECT FirstName, LastName, Gender, Salary,
+--COUNT(Gender) OVER (PARTITION BY Gender) AS TotalGender,
+--AVG(Salary) OVER (PARTITION BY Gender) AS AvgSalary
+--FROM EmployeeDemographics emp
+--JOIN EmployeeSalary sal
+--ON emp.EmployeeID = sal.EmployeeID
+--WHERE Salary > '45000')
+--SELECT *
+--FROM CTE_Employee;
